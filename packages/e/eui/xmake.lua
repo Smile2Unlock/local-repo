9cb019f43070500e759c318924fd440cfecd73df
ation_ciger <ation_ciger@sgxi.cn>
2026-04-21 01:19:04 +0800

    Add local EUI and reflect-cpp packages

---
+++ packages/e/eui/xmake.lua
@@ -0,0 +1,140 @@
+package("eui")
+    set_homepage("https://github.com/sudoevolve/EUI")
+    set_description("EUI core library packaged from the upstream CMake project")
+    set_license("MIT")
+
+    add_urls("https://github.com/sudoevolve/EUI.git")
+    add_versions("df3f9b591920535789a34426422134a3e92b23b1", "df3f9b591920535789a34426422134a3e92b23b1")
+
+    add_deps("cmake")
+
+    local function _normalize_binpath(bin)
+        if not bin then
+            return nil
+        end
+        if os.isfile(bin) then
+            return path.unix(bin)
+        end
+        if is_host("windows") and os.isfile(bin .. ".exe") then
+            return path.unix(bin .. ".exe")
+        end
+        return path.unix(bin)
+    end
+
+    local function _collect_includedirs(installdir)
+        local includedirs = {}
+        local include_root = path.join(installdir, "include")
+        if os.isdir(include_root) then
+            table.insert(includedirs, include_root)
+            for _, subdir in ipairs(os.dirs(path.join(include_root, "*"))) do
+                table.insert(includedirs, subdir)
+            end
+        end
+        return includedirs
+    end
+
+    on_load(function (package)
+        package:add("links", "eui_neo_core")
+    end)
+
+    on_install(function (package)
+        local installdir = package:installdir()
+        local srcdir = package:sourcedir()
+        local builddir = path.join(package:builddir(), "build")
+        local includedir = path.join(installdir, "include")
+        local eui_includedir = path.join(includedir, "eui")
+        local libdir = path.join(installdir, "lib")
+        local bindir = path.join(installdir, "bin")
+
+        os.mkdir(includedir)
+        os.mkdir(eui_includedir)
+        os.mkdir(libdir)
+        os.mkdir(bindir)
+
+        local configs = {
+            CMAKE_BUILD_TYPE = package:is_debug() and "Debug" or "Release"
+        }
+
+        local cc = _normalize_binpath(package:build_getenv("cc"))
+        local cxx = _normalize_binpath(package:build_getenv("cxx"))
+        if cc then
+            configs.CMAKE_C_COMPILER = cc
+        end
+        if cxx then
+            configs.CMAKE_CXX_COMPILER = cxx
+        end
+
+        import("package.tools.cmake").install(package, configs, {builddir = builddir, sourcedir = srcdir})
+
+        if os.isfile(path.join(srcdir, "src", "EUINEO.h")) then
+            os.cp(path.join(srcdir, "src", "EUINEO.h"), eui_includedir)
+        end
+        for _, subdir in ipairs({"app", "components", "pages", "ui"}) do
+            local source_subdir = path.join(srcdir, "src", subdir)
+            if os.isdir(source_subdir) then
+                os.cp(source_subdir, eui_includedir)
+            end
+        end
+        if os.isfile(path.join(srcdir, "third_party", "stb_truetype.h")) then
+            os.cp(path.join(srcdir, "third_party", "stb_truetype.h"), eui_includedir)
+        end
+
+        local function copy_if_exists(filepath, destdir)
+            if os.isfile(filepath) then
+                os.cp(filepath, destdir)
+                return true
+            end
+            return false
+        end
+
+        local function copy_candidates(filenames, destdir)
+            local copied = false
+            for _, filename in ipairs(filenames) do
+                copied = copy_if_exists(path.join(builddir, filename), destdir) or copied
+                for _, filepath in ipairs(os.files(path.join(builddir, "**/" .. filename))) do
+                    os.cp(filepath, destdir)
+                    copied = true
+                end
+            end
+            return copied
+        end
+
+        copy_candidates({"libeui_neo_core.a", "eui_neo_core.lib", "libeui_neo_core.so", "libeui_neo_core.dylib"}, libdir)
+        copy_candidates({"eui_neo_core.dll"}, bindir)
+
+        local found_lib = false
+        for _, candidate in ipairs({
+            path.join(libdir, "libeui_neo_core.a"),
+            path.join(libdir, "eui_neo_core.lib"),
+            path.join(libdir, "libeui_neo_core.so"),
+            path.join(libdir, "libeui_neo_core.dylib"),
+            path.join(bindir, "eui_neo_core.dll")
+        }) do
+            if os.isfile(candidate) then
+                found_lib = true
+                break
+            end
+        end
+
+        if not found_lib then
+            raise("eui package: failed to locate built eui_neo_core artifacts")
+        end
+    end)
+
+    on_fetch(function (package)
+        local result = {}
+        result.links = {"eui_neo_core"}
+        result.linkdirs = package:installdir("lib")
+        result.includedirs = _collect_includedirs(package:installdir())
+        result.bindirs = package:installdir("bin")
+        return result
+    end)
+
+    on_test(function (package)
+        assert(os.isfile(path.join(package:installdir("lib"), "libeui_neo_core.a"))
+            or os.isfile(path.join(package:installdir("lib"), "eui_neo_core.lib"))
+            or os.isfile(path.join(package:installdir("lib"), "libeui_neo_core.so"))
+            or os.isfile(path.join(package:installdir("lib"), "libeui_neo_core.dylib"))
+            or os.isfile(path.join(package:installdir("bin"), "eui_neo_core.dll")),
+            "eui package: installed core library artifact is missing")
+    end)
---
+++ packages/s/r/reflect-cpp/xmake.lua
@@ -0,0 +1,203 @@
+package("reflect-cpp")
+    set_homepage("https://github.com/getml/reflect-cpp")
+    set_description("A C++20 library for fast serialization, deserialization and validation using reflection")
+    set_license("MIT")
+
+    add_urls("https://github.com/getml/reflect-cpp/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/getml/reflect-cpp.git", {submodules = false})
+
+    add_versions("v0.24.0", "2185d45cca58f60bcdaca37a995c6f8e90c0105f312610333fed75f2efa6a996")
+    add_versions("v0.22.0", "5756d74e7df640b4633a3ea5a3c0d7c4e096bdd3f67828f8b02f58b156ba39ec")
+    add_versions("v0.20.0", "b774f11fd602683e3c7febabfe6e888b866cec28497c5e9c6ba82aeeb4465bbc")
+    add_versions("v0.19.0", "aad9e010a0e716ecf643a95cec2047c74ce4311accfe42b4cf888672267ab8cd")
+    add_versions("v0.18.0", "c8df46550d787105ce695ea8f99425dc47475f5377c5253d412dd63f622dc7c7")
+    add_versions("v0.17.0", "08b6406cbe4c6c14ff1a619fe93a94f92f6d9eb22213d93529ad975993945e45")
+    add_versions("v0.16.0", "a84d94dbd353d788926d6e54507b44c046863f7bc4ecb35afe0338374a68a77d")
+    add_versions("v0.14.1", "639aec9d33025703a58d32c231ab1ab474c0cc4fb0ff90eadcaffb49271c41cd")
+    add_versions("v0.14.0", "ea92a2460a71184b7d4fa4e9baad9910efad092df78b114459a7d6b0ee558d3c")
+    add_versions("v0.13.0", "a7a31832fe8bbaa7f7299da46dfd4ccc8b99a13242e16a1d93f8669de1fca9c6")
+    add_versions("v0.12.0", "13d448dd5eaee13ecb7ab5cb61cb263c7111ba75230503adc823a888f68e1eaa")
+    add_versions("v0.11.1", "e45f112fb3f14507a4aa53b99ae2d4ab6a4e7b2d5f04dd06fec00bf7faa7bbdc")
+    add_versions("v0.10.0", "d2c8876d993ddc8c57c5804e767786bdb46a2bdf1a6cd81f4b14f57b1552dfd7")
+
+    add_configs("bson", {description = "Enable BSON support.", default = false, type = "boolean", readonly = true})
+    add_configs("yyjson", {description = "Enable yyjson support.", default = true, type = "boolean"})
+    add_configs("cbor", {description = "Enable CBOR support.", default = false, type = "boolean"})
+    add_configs("capnproto", {description = "Enable Capnproto support.", default = false, type = "boolean"})
+    add_configs("flatbuffers", {description = "Enable Flexbuffers support.", default = false, type = "boolean"})
+    add_configs("msgpack", {description = "Enable Msgpack support.", default = false, type = "boolean"})
+    add_configs("xml", {description = "Enable XML support.", default = false, type = "boolean"})
+    add_configs("toml", {description = "Enable TOML support.", default = true, type = "boolean"})
+    add_configs("yaml", {description = "Enable YAML support.", default = false, type = "boolean"})
+    add_configs("ubjson", {description = "Enable UBJSON support.", default = false, type = "boolean"})
+    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean"})
+
+    on_check(function (package)
+        if package:is_plat("windows") then
+            local vs = package:toolchain("msvc"):config("vs")
+            assert(vs and tonumber(vs) >= 2022, "package(reflect-cpp): need vs >= 2022")
+        else
+            assert(package:check_cxxsnippets({test = [[
+                #include <ranges>
+                #include <source_location>
+                #include <iostream>
+                #include <string_view>
+                #include <cctype>
+                void test() {
+                    constexpr std::string_view message = "Hello, C++20!";
+                    for (char c : std::views::filter(message, [](char ch) { return std::islower(static_cast<unsigned char>(ch)); })) {
+                        std::cout << std::source_location::current().line() << ": " << c << '\n';
+                    }
+                }
+            ]]}, {configs = {languages = "c++20"}}), "package(reflect-cpp): requires at least C++20.")
+        end
+
+        if package:config("capnproto") and package:version() and package:version():le("0.16.0") then
+            raise("package(reflect-cpp): capnproto is not supported for version 0.16.0 and below.")
+        end
+    end)
+
+    on_load(function (package)
+        local version = package:version()
+
+        if package:config("yyjson") then
+            package:add("deps", "yyjson")
+        end
+        if package:config("cbor") then
+            package:add("deps", "tinycbor")
+        end
+        if package:config("capnproto") then
+            package:add("deps", "capnproto")
+        end
+        if package:config("flatbuffers") then
+            package:add("deps", "flatbuffers")
+        end
+        if package:config("msgpack") then
+            package:add("deps", "msgpack-c")
+        end
+        if package:config("xml") then
+            package:add("deps", "pugixml")
+        end
+        if package:config("toml") then
+            package:add("deps", "toml++")
+        end
+        if package:config("yaml") then
+            package:add("deps", "yaml-cpp")
+        end
+        if package:config("ubjson") then
+            package:add("deps", "jsoncons")
+        end
+
+        if version then
+            if version:lt("0.13.0") then
+                package:set("kind", "library", {headeronly = true})
+            end
+            if version:ge("0.11.1") then
+                package:add("deps", "ctre", {configs = {cmake = true}})
+                if version:eq("0.11.1") then
+                    package:add("defines", "REFLECTCPP_NO_BUNDLED_DEPENDENCIES")
+                end
+            end
+        end
+
+        if package:gitref() or (version and (version:lt("0.11.1") or version:ge("0.13.0"))) then
+            package:add("deps", "cmake")
+        end
+    end)
+
+    on_install(function (package)
+        local version = package:version()
+        if package:gitref() or (version and (version:lt("0.11.1") or version:ge("0.13.0"))) then
+            local configs = {
+                "-DREFLECTCPP_USE_BUNDLED_DEPENDENCIES=OFF",
+                "-DREFLECTCPP_USE_VCPKG=OFF",
+                "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"),
+                "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"),
+                "-DREFLECTCPP_BSON=" .. (package:config("bson") and "ON" or "OFF"),
+                "-DREFLECTCPP_CBOR=" .. (package:config("cbor") and "ON" or "OFF"),
+                "-DREFLECTCPP_CAPNPROTO=" .. (package:config("capnproto") and "ON" or "OFF"),
+                "-DREFLECTCPP_FLEXBUFFERS=" .. (package:config("flatbuffers") and "ON" or "OFF"),
+                "-DREFLECTCPP_MSGPACK=" .. (package:config("msgpack") and "ON" or "OFF"),
+                "-DREFLECTCPP_XML=" .. (package:config("xml") and "ON" or "OFF"),
+                "-DREFLECTCPP_TOML=" .. (package:config("toml") and "ON" or "OFF"),
+                "-DREFLECTCPP_UBJSON=" .. (package:config("ubjson") and "ON" or "OFF"),
+                "-DREFLECTCPP_YAML=" .. (package:config("yaml") and "ON" or "OFF")
+            }
+            import("package.tools.cmake").install(package, configs)
+        else
+            os.rm("include/thirdparty")
+            os.cp("include", package:installdir())
+        end
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <rfl.hpp>
+            #include <rfl/json.hpp>
+            #include <rfl/DefaultIfMissing.hpp>
+            struct Person {
+                std::string first_name;
+                std::string last_name;
+                int age;
+            };
+            void test() {
+                const auto homer = Person{.first_name = "Homer", .last_name = "Simpson", .age = 45};
+                const std::string json_string = rfl::json::write(homer);
+                auto homer2 = rfl::json::read<Person, rfl::DefaultIfMissing>(json_string).value();
+                (void)homer2;
+            }
+        ]]}, {configs = {languages = "c++20"}}))
+
+        if package:config("toml") then
+            assert(package:check_cxxsnippets({test = [[
+                #include <rfl.hpp>
+                #include <rfl/toml.hpp>
+                struct AppConfig {
+                    std::string theme;
+                    int width;
+                };
+                void test() {
+                    const auto cfg = AppConfig{.theme = "light", .width = 1280};
+                    const auto toml_str = rfl::toml::write(cfg);
+                    auto cfg2 = rfl::toml::read<AppConfig>(toml_str).value();
+                    (void)cfg2;
+                }
+            ]]}, {configs = {languages = "c++20"}}))
+        end
+
+        if package:config("msgpack") then
+            assert(package:check_cxxsnippets({test = [[
+                #include <rfl.hpp>
+                #include <rfl/msgpack.hpp>
+                struct Person {
+                    std::string first_name;
+                    std::string last_name;
+                    int age;
+                };
+                void test() {
+                    const auto homer = Person{.first_name = "Homer", .last_name = "Simpson", .age = 45};
+                    auto payload = rfl::msgpack::write(homer);
+                    auto homer2 = rfl::msgpack::read<Person>(payload).value();
+                    (void)homer2;
+                }
+            ]]}, {configs = {languages = "c++20"}}))
+        end
+
+        if package:config("capnproto") then
+            assert(package:check_cxxsnippets({test = [[
+                #include <rfl.hpp>
+                #include <rfl/capnproto.hpp>
+                struct Person {
+                    std::string first_name;
+                    std::string last_name;
+                    int age;
+                };
+                void test() {
+                    const auto homer = Person{.first_name = "Homer", .last_name = "Simpson", .age = 45};
+                    auto payload = rfl::capnproto::write(homer);
+                    auto homer2 = rfl::capnproto::read<Person>(payload).value();
+                    (void)homer2;
+                }
+            ]]}, {configs = {languages = "c++20"}}))
+        end
+    end)
